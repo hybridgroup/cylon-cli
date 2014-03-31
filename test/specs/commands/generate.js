d191ccd30e4008eee2ae1a4839d23455467c987b
Andrew Stewart <andrew@stwrt.ca>
2014-03-31 11:57:54 -0700

    Modify 'generate' to support multiple targets

--- test/specs/commands/generate.js
+++ test/specs/commands/generate.js
@@ -46,67 +46,85 @@ describe("cylon generate", function() {
       globule.find.restore();
     });
 
-    context("when a name is supplied", function() {
-      var args = ["new-hardware"];
-      var template = "<%= adaptorName %> <%= adaptorClassName %> <%= basename %> <%= cylonVersion %>";
-
-      beforeEach(function() {
-        globule.find.returns(["adaptorName.js.tpl"]);
-        fs.readFileSync.returns(template);
-        path.join.returns("template_dir")
-        module.action(args);
-      });
-
-      it("logs that it's creating a new module", function() {
-        expect(console.log).to.be.calledWith("Creating new module 'cylon-new-hardware'");
-      });
-
-      it("copies over the template using process#exec and cp", function() {
-        expect(path.join).to.be.calledWithMatch('', 'support/generate');
-        expect(process.exec).to.be.calledWith("cp -R template_dir ./cylon-new-hardware");
-      });
-
-      it("logs that it's compiling the templates for the new module", function() {
-        expect(console.log).to.be.calledWith("Compiling templates for 'cylon-new-hardware'");
-      });
-
-      it("uses globule to find all template files", function() {
-        expect(globule.find).to.be.calledWith("./cylon-new-hardware/**/*.tpl");
-      });
-
-      describe("with template files", function() {
-        it("renames the file", function() {
-          expect(fs.renameSync).to.be.calledWith("adaptorName.js.tpl", "cylon-new-hardware.js");
-        });
-
-        it("reads the new file", function() {
-          expect(fs.readFileSync).to.be.calledWith("cylon-new-hardware.js");
-        });
-
-        it("runs EJS on the template to get the compiled file", function() {
-          expect(ejs.render).to.be.calledWith(template);
-        });
-
-        it("writes the compiled template to the new file", function() {
-          var compiled = "cylon-new-hardware NewHardware new-hardware"
-          expect(fs.writeFileSync).to.be.calledWithMatch("cylon-new-hardware.js", compiled);
-        });
-      });
-    });
-
-    context("when no name is supplied", function() {
+    describe("when no generator is supplied", function() {
       var args = [];
 
       beforeEach(function() {
         module.action(args);
       });
 
-      it("logs that no name was supplied", function() {
-        expect(console.log).to.be.calledWith("No name supplied.")
+      it("prints that no generator was supplied", function() {
+        expect(console.log).to.be.calledWith("Invalid/no generator supplied")
       });
 
       it("doesn't run any commands", function() {
         expect(process.exec).to.not.be.called;
+      });
+    });
+
+    describe("module", function() {
+      context("when a name is supplied", function() {
+        var args = ["module", "new-hardware"];
+        var template = "<%= adaptorName %> <%= adaptorClassName %> <%= basename %> <%= cylonVersion %>";
+
+        beforeEach(function() {
+          globule.find.returns(["adaptorName.js.tpl"]);
+          fs.readFileSync.returns(template);
+          path.join.returns("template_dir")
+          module.action(args);
+        });
+
+        it("logs that it's creating a new module", function() {
+          expect(console.log).to.be.calledWith("Creating new module 'cylon-new-hardware'");
+        });
+
+        it("copies over the template using process#exec and cp", function() {
+          expect(path.join).to.be.calledWithMatch('', 'support/generate');
+          expect(process.exec).to.be.calledWith("cp -R template_dir ./cylon-new-hardware");
+        });
+
+        it("logs that it's compiling the templates for the new module", function() {
+          expect(console.log).to.be.calledWith("Compiling templates for 'cylon-new-hardware'");
+        });
+
+        it("uses globule to find all template files", function() {
+          expect(globule.find).to.be.calledWith("./cylon-new-hardware/**/*.tpl");
+        });
+
+        describe("with template files", function() {
+          it("renames the file", function() {
+            expect(fs.renameSync).to.be.calledWith("adaptorName.js.tpl", "cylon-new-hardware.js");
+          });
+
+          it("reads the new file", function() {
+            expect(fs.readFileSync).to.be.calledWith("cylon-new-hardware.js");
+          });
+
+          it("runs EJS on the template to get the compiled file", function() {
+            expect(ejs.render).to.be.calledWith(template);
+          });
+
+          it("writes the compiled template to the new file", function() {
+            var compiled = "cylon-new-hardware NewHardware new-hardware"
+            expect(fs.writeFileSync).to.be.calledWithMatch("cylon-new-hardware.js", compiled);
+          });
+        });
+      });
+
+      context("when no name is supplied", function() {
+        var args = ["module"];
+
+        beforeEach(function() {
+          module.action(args);
+        });
+
+        it("logs that no name was supplied", function() {
+          expect(console.log).to.be.calledWith("No name supplied.")
+        });
+
+        it("doesn't run any commands", function() {
+          expect(process.exec).to.not.be.called;
+        });
       });
     });
   });
